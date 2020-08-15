1edd846fcd7e83dbaa955645de524b1be1002cdd
Mauro <maurogheco@gmail.com>
2020-08-15 02:07:49 -0300

    Updates

---
+++ precache-manifest.6df8e3c39e01ba0e5a22bc0482c3116d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e581df6f852d671ca13ae76fbd43fe9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2eaaf87377fd8213b7fa",
+    "url": "./static/css/main.32855b4f.chunk.css"
+  },
+  {
+    "revision": "8851be02a6854fa515eb",
+    "url": "./static/js/2.cc46d40d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.cc46d40d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2eaaf87377fd8213b7fa",
+    "url": "./static/js/main.f5aa64e5.chunk.js"
+  },
+  {
+    "revision": "1de6ec5946f991259056",
+    "url": "./static/js/runtime-main.11d4fa65.js"
+  }
+]);
